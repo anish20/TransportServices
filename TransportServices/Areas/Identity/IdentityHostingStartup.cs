5c0585c19f646fb74f7626a4a77f47dd2c5f1600
Your Name <anish15.786@gmail.com>
2019-05-26 02:09:31 +0530

    Project Uploaded Successfully..

---
+++ TransportServices/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TransportServices.Data;
+using TransportServices.Models;
+
+[assembly: HostingStartup(typeof(TransportServices.Areas.Identity.IdentityHostingStartup))]
+namespace TransportServices.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
---
+++ TransportServices/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TransportServices.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
