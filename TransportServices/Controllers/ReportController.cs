3717b3641d16cde1a0def8d58f4815e2b173dcfe
anish20 <anish15.786@gmail.com>
2019-05-31 00:50:54 +0530

    changes for report page

---
+++ TransportServices/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TransportServices.Models;
+
+namespace TransportServices.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+
+
+    }
+}
